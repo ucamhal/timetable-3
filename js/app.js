7e90b7618a8f2b7ec7f1f251568e0c6f70899629
Tim De Groote <td324@td324-mbp.local>
2012-10-15 17:45:38 +0100

    populated results list, added calendar placeholder image

--- js/app.js
+++ js/app.js
@@ -1,3 +1,17 @@
 $(function () {
 	"use strict";
+
+	$('a[href="#"]').click(function (event) {
+		event.preventDefault();
+	});
+
+	//populate results list
+	(function populateResults(toAdd) {
+		var i,
+			$singleResult = $('#search #results ul li');
+
+		for (i = 0; i < toAdd; i += 1) {
+			$('#search #results ul').append($singleResult.clone());
+		}
+	}(12));
 });
