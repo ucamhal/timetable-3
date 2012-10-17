a51a31dd0f1e75d6a7b411c558908e918995e8e5
Ian Boston <ieb@tfd.co.uk>
2012-10-17 17:39:19 +1100

    Added ical importer

--- app/timetables/utils/formats/ical.py
+++ app/timetables/utils/formats/ical.py
@@ -5,6 +5,11 @@ Created on Oct 17, 2012
 '''
 from icalendar import Event as iCalEvent
 from django.http import HttpResponse
+from icalendar.cal import Calendar, Alarm
+from timetables.models import Event
+import datetime
+from django.utils.timezone import utc
+from timetables.utils.date import DateConverter
 
 
 class ICalExporter(object):
@@ -26,21 +31,21 @@ class ICalExporter(object):
             for e in events:
                 event = iCalEvent()
                 event.add('summary', '%s' % e.title)
-                event.add('dtstart', e.start)
-                event.add('dtend', e.end)
+                event.add('dtstart', DateConverter.from_datetime(e.start, e.metadata.get("x-allday")))
+                event.add('dtend', DateConverter.from_datetime(e.end, e.metadata.get("x-allday")))
                 event.add('location', e.location)
                 # If a mapping has been provided, unpack
+                metadata = e.metadata
                 if metadata_names is not None:
-                    metadata = e.metadata
                     for metadata_name, icalname in metadata_names.iteritems():
-                        if metadata_name in metadata:
+                        if metadata_name in metadata_names:
                             o = metadata[metadata_name]
                             if isinstance(o, list):
                                 for e in o:
                                     event.add(icalname,e)
                             else:
                                 event.add(icalname,o)
-                event['uid'] = '%s@timetables.caret.cam.ac.uk' % e.id
+                event["uid"] = e.uid
                 event.add('priority', 5)
                 yield event.as_string()
             yield "END:VCALENDAR\r\n"
@@ -48,3 +53,36 @@ class ICalExporter(object):
         response['Content-Disposition'] = "attachment; filename=%s.ics" % feed_name
         response.streaming = True
         return response
+
+
+class ICalImporter(object):
+
+    def _safe_get(self, calcomp, key, default_value):
+        try:
+            return calcomp.decoded(key)
+        except:
+            return default_value
+
+    def import_events(self, source, feedfile):
+        Event.objects.filter(source=source).delete()
+        iCalEvent.ignore_exceptions = False
+        Alarm.ignore_exceptions = True
+        cal = Calendar.from_ical(feedfile.read())
+        nevents = 0
+        for e in cal.walk('VEVENT'):
+            event = Event(start=DateConverter.to_datetime(e.decoded('DTSTART')),
+                          end=DateConverter.to_datetime(e.decoded('DTEND')),
+                          location=self._safe_get(e, "LOCATION", ""),
+                          title=self._safe_get(e, "SUMMARY", ""),
+                          uid=self._safe_get(e, "UID", ""),
+                          source=source)
+            metadata = event.metadata
+            for k,v in e.iteritems():
+                metadata[k] = "%s" % e[k]
+
+            metadata['x-allday'] = DateConverter.is_date(e.decoded('DTSTART')),
+            event.save()
+            nevents = nevents + 1
+        return nevents
+
+
