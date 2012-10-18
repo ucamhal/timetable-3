99bd1e68ba9225a9d9ee7fd0ef828a33bd75e88c
Hal Blackburn <hwtb2@caret.cam.ac.uk>
2012-10-18 17:18:24 +0100

    Added subject choices directly into index.html for Tim.

--- app/timetables/views/__init__.py
+++ app/timetables/views/__init__.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
 
+from timetables import models
+
+
 def index(request):
-    return render(request, "index.html")
+    return render(request, "index.html", {"subjects": models.subjects()})
