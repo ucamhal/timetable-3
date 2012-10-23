9ee3a066af41762a7236da8fe06b448e7e34afd7
Hal Blackburn <hwtb2@caret.cam.ac.uk>
2012-10-23 12:41:29 +0100

    Updated subject list to contain names of Triposes without actual subjects.
    [Hal & Geoff]

--- app/timetables/views/indexview.py
+++ app/timetables/views/indexview.py
@@ -4,9 +4,12 @@ Created on Oct 19, 2012
 @author: ieb
 '''
 from django.shortcuts import render
-from timetables.models import Thing
 from django.views.generic.base import View
-from timeit import itertools
+from django.db.models import Q
+
+from timetables.models import Thing
+
+import itertools
 
 class IndexView(View):
 
@@ -15,6 +18,7 @@ class IndexView(View):
         """
 
         FIXME: Note: This desperately needs to be generalized to avoid hard coding the structure of the data into the code.
+               Note: This will be moved into a haystack query at some point so this code doesn't matter much.
 
         Gets a sequence of all subjects.
 
@@ -33,22 +37,31 @@ class IndexView(View):
         # Fetch subject name, subject id, subject's parent level name and subject's parent level's parent tripos name
         subject_values = (Thing.objects.filter(type__in=["subject", "experimental", "option"])
                 .order_by("fullname", "parent__parent__fullname", "parent__fullname")
-                .values("fullname", "fullpath", "parent__fullname", "parent__parent__fullname"))
+                .values_list("fullname", "fullpath", "parent__fullname", "parent__parent__fullname"))
+
+        return self._collect_subjects(subject_values)
+
+    def _collect_subjects(self, subjects):
+        def key(values):
+            if len(values) == 4:
+                return (values[0], values[3])
+            return (values[0], None)
 
         # Group together subjects under the same tripos with the same name.
-        for _, subjects in itertools.groupby(subject_values,
-                lambda s: (s["fullname"], s["parent__parent__fullname"])):
+        for _, subjects in itertools.groupby(subjects, key):
             tripos_name = None
             subject_name = None
             fullpaths_by_level = []
 
             for subject in subjects:
-                tripos_name = tripos_name or subject["parent__parent__fullname"]
-                subject_name = subject_name or subject["fullname"]
+                if len(subject) == 3:
+                    subject_name, path, level = subject
+                else:
+                    subject_name, path, level, tripos_name = subject
 
                 fullpaths_by_level.append({
-                    "fullpath": subject["fullpath"],
-                    "level_name": subject["parent__fullname"]
+                    "fullpath": path,
+                    "level_name": level
                 })
 
             yield {
@@ -57,10 +70,25 @@ class IndexView(View):
                 "fullpaths_by_level": fullpaths_by_level
             }
 
+    def _tripos_subjects(self):
+        subject_triposes = (Thing.objects.filter(
+                ~Q(thing__thing__type="subject"), type="tripos", thing__type="part")
+                .values_list("fullname", "thing__fullpath", "thing__fullname"))
+
+        return self._collect_subjects(subject_triposes)
+
+    def _all_subjects(self):
+        def key(subject):
+            return (subject["subject_name"], subject["tripos_name"])
+
+        return sorted(
+                itertools.chain(self._subjects(), self._tripos_subjects()),
+                key=key)
+
     
     def get(self, request):
         context = {
-                   "subjects": self._subjects(),
+                   "subjects": self._all_subjects(),
                    "user" : request.user
                    }
         return render(request, "index.html", context)
