ddb991922491a7986ff7b22d9a0ecc1f3a16ecc8
Hal Blackburn <hwtb2@caret.cam.ac.uk>
2012-10-24 12:43:21 +0100

    Re-added in_users_timetable and in_range filters to the Event queryset.
    (Taken from Timetables 2).

--- app/timetables/models.py
+++ app/timetables/models.py
@@ -6,6 +6,7 @@ import os
 import time
 
 from django.db import models
+from django.db.models import query
 from django.db.models.signals import pre_save
 from django.conf import settings
 from django.utils import simplejson as json
@@ -290,7 +291,48 @@ class Event(SchemalessModel):
         SchemalessModel._prepare_save(sender,**kwargs)
         instance = kwargs['instance']
         if instance.uid is None or instance.uid == "":
-            instance.uid = HierachicalModel.hash("%s@%s" % (time.time(), settings.INSTANCE_NAME)) 
+            instance.uid = HierachicalModel.hash("%s@%s" % (time.time(), settings.INSTANCE_NAME))
+    
+    
+    class QuerySet(query.QuerySet):
+        def in_users_timetable(self, user):
+            """
+            Filter the queryset to contain events in the personal timetable of
+            the specified user.
+            
+            Args:
+                user: The username or User instance of the user whose timetable
+                    the events should be in.
+            """
+            if isinstance(user, basestring):
+                username = user
+            else:
+                username = user.username
+            
+            return self.filter(source__eventsourcetag__thing__name=username,
+                    source__eventsourcetag__thing__type="user")
+        
+        def in_range(self, start, end):
+            """
+            Filters the queryset to contain events intersecting the specified
+            start and end dates.
+            """
+            if start > end:
+                raise ValueError(
+                        "start was > end. start: %s, end: %s" % (start, end))
+            
+            # Note: it's assumed that event's timeslots's start & end are
+            # correctly sorted...
+            ends_before_start= models.Q(end__lte=start)
+            starts_after_end= models.Q(start__gte=end)
+            
+            outside_range = ends_before_start | starts_after_end
+            inside_range = ~outside_range
+            
+            return self.filter(inside_range)
+        
+        def include_series_length(self):
+            return self.annotate(Count("owning_series__event"))
 
         
 pre_save.connect(Event._pre_save, sender=Event)
