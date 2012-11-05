5f85ad624e475b73a3570089b06738722aac0654
Hal Blackburn <hwtb2@caret.cam.ac.uk>
2012-11-05 18:03:28 +0000

    Re-added utils as app. This is required to run util's unit tests.

--- app/timetables/settings.py
+++ app/timetables/settings.py
@@ -238,6 +238,7 @@ INSTALLED_APPS = (
     # enable the haystack app so that we can index things.
     'haystack',
     'timetables',
+    'timetables.utils',
     'south', # For schema migration, easy_install South to use.
 )
 
