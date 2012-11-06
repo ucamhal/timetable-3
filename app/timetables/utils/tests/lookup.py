b4f8eefa7e58b5ef30a5876938e69135d6d64386
Ian Boston <ieb@tfd.co.uk>
2012-11-07 08:22:00 +1100

    reverted changes to lookup.py and made test look directly for configuration to determin if it should run.

--- app/timetables/utils/tests/lookup.py
+++ app/timetables/utils/tests/lookup.py
@@ -1,11 +1,16 @@
 from django.test import TestCase
 from timetables.utils import lookup
-from timetables.utils.lookup import ldap_is_available
+from django.conf import settings
+
+try:
+    LDAP_LOOKUP_URL = settings.LDAP_LOOKUP_URL
+except:
+    LDAP_LOOKUP_URL = None
 
 class LookupTest(TestCase):
 
     def testHalExistsInLookup(self):
-        if ldap_is_available():
+        if LDAP_LOOKUP_URL is not None:
             with lookup.Lookup() as l:
                 hal = l.get_user("hwtb2")
                 self.assertTrue(hal, "hwtb2 should exist in Lookup")
