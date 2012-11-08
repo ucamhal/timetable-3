8d679f66c73363f3aa93c97b284c6368f7e55f10
Ian Boston <ieb@tfd.co.uk>
2012-11-08 11:00:50 +1100

    Added transaction wrapper to the post request (always required), and commented out permissions checks. The permissions checks need to operate on paths not IDs so they are not enabled at present. To enable module_id needs to be converted to a path. This will also ensure that edits are made to the current version of the Thing (if we enable versioning in the future). Should check that for editing events and event sources which are ID based.

--- app/timetables/views/moduleeditform.py
+++ app/timetables/views/moduleeditform.py
@@ -6,6 +6,8 @@ from django import shortcuts
 
 from timetables.models import Thing
 from timetables.forms import ModuleForm
+from django.utils.decorators import method_decorator
+from timetables.utils.xact import xact
 
 
 class ModuleEditFormView(View):
@@ -15,21 +17,29 @@ class ModuleEditFormView(View):
     
     def get(self, request, module_id):
         if module_id is None:
-            return HttpResponseNotFound()
-        
-        # TODO - permission checks
-        
+            return HttpResponseBadRequest()
+        # FIXME: We need to convert the above to be path based to be able to perform permissions checks.
+        # we cant do it based on id without getting the item, which is why it needs to be path based.
+        # Once that is done the permissions checks can be enabled.
+        # if not request.user.has_perm(Thing.PERM_READ,ThingSubject(fullpath=module_id)):
+        #    return HttpResponseForbidden()
+
         module = shortcuts.get_object_or_404(Thing, id=module_id) 
         if module.type == "module": # need to test object is module
             return self._render_form(request, ModuleForm(instance=module))
         else:
             return HttpResponseNotFound() # maybe ...
         
+    @method_decorator(xact)
     def post(self, request, module_id):
         if module_id is None:
             return HttpResponseBadRequest("Creating modules not yet supported.")
-        
-        # TODO - permission checks
+        # FIXME: We need to convert the above to be path based to be able to perform permissions checks.
+        # we cant do it based on id without getting the item, which is why it needs to be path based.
+        # Once that is done the permissions checks can be enabled.
+        # if not request.user.has_perm(Thing.PERM_WRITE,ThingSubject(fullpath=module_id)):
+        #    return HttpResponseForbidden()
+
         
         module = shortcuts.get_object_or_404(Thing, id=module_id)
         if module.type == "module":
