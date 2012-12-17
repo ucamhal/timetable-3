709f1b3d2ae440e873069d210d0e7249eaa11eaf
Hal Blackburn <hwtb2@caret.cam.ac.uk>
2012-12-17 14:50:20 +0000

    Test lock creation

--- app/timetables/tests/locking.py
+++ app/timetables/tests/locking.py
@@ -1,7 +1,6 @@
-import datetime
-
 from django.test import TestCase
 from django.core import exceptions
+from django.utils import timezone
 
 from timetables import models
 
@@ -14,7 +13,22 @@ class TestThingLock(TestCase):
         try:
             # Try to create a ThingLock with a non-user as the owner
             models.ThingLock.objects.create(thing=asncp1, owner=asncp1,
-                    expires=datetime.datetime.now(), name="short")
+                    expires=timezone.now(), name="short")
             self.fail("The previous call should have failed.")
         except exceptions.ValidationError:
             pass
+
+    def test_lock_creation(self):
+        hal = models.Thing.objects.get(fullpath="user/hal")
+        asncp1 = models.Thing.objects.get(fullpath="tripos/asnc/I")
+
+        lock = models.ThingLock.objects.create(thing=asncp1, owner=hal,
+                    expires=timezone.now(), name="short")
+        lock.save()
+
+        self.assertTrue(asncp1 == lock.thing)
+        self.assertTrue(hal == lock.owner)
+        self.assertTrue(asncp1 in hal.locked_things.all())
+        self.assertTrue(hal in asncp1.locked_by.all())
+        self.assertTrue(lock in hal.owned_locks.all())
+        self.assertTrue(lock in asncp1.locks.all())
