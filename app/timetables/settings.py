659c18bce9fefb18a200449eeeffc7c0b9d28580
Raymond Chan <raymond.chan@caret.cam.ac.uk>
2013-04-26 14:32:11 +0100

    Rename db settings to "timetables3"

--- app/timetables/settings.py
+++ app/timetables/settings.py
@@ -82,9 +82,9 @@ else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-            'NAME': 'timetables2', # Or path to database file if using sqlite3.
-            'USER': 'timetables2', # Not used with sqlite3.
-            'PASSWORD': 'timetables2', # Not used with sqlite3.
+            'NAME': 'timetables3', # Or path to database file if using sqlite3.
+            'USER': 'timetables3', # Not used with sqlite3.
+            'PASSWORD': 'timetables3', # Not used with sqlite3.
             'HOST': 'localhost', # Set to empty string for localhost. Not used with sqlite3.
             'PORT': '5432', # Set to empty string for default. Not used with sqlite3.
             'OPTIONS': {
