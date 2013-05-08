36d08862e8dae58e8bb8680c29c5065e6c29ff29
Tim De Groote <td324@caret.cam.ac.uk>
2013-05-08 14:19:00 +0100

    Added build tasks, exported jshint options into .jshintrc so it isn't exclusively available to grunt, updated README.install

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,38 +1,90 @@
+/* global module */
 module.exports = function (grunt) {
+    "use strict";
 
-    var jsBasePath = 'static-data/static/js/',
+    /**
+     * 'grunt jshint' validates all javascript we've written ourselves
+     * 
+     * 'grunt watch:js' runs grunt jshint whenever a javascript file is saved
+     * 
+     * 'grunt build' optimizes and minifies all js/css/img into the
+     * static-data/build folder
+     *
+     * 'grunt requirejs' only optimize/minify the js files
+     *
+     * 'grunt cssmin' only optimize/minify the css files
+     *
+     * 'grunt imagemin' only optimize the images
+     */
+
+
+    grunt.loadNpmTasks("grunt-contrib-watch");
+    grunt.loadNpmTasks("grunt-contrib-jshint");
+    grunt.loadNpmTasks("grunt-contrib-requirejs");
+    grunt.loadNpmTasks("grunt-contrib-cssmin");
+    grunt.loadNpmTasks("grunt-contrib-imagemin");
+
+    var jsBasePath = "static-data/static/js/",
         jsFiles = [
-            jsBasePath + '*.js',
-            jsBasePath + 'view/**/*.js',
-            jsBasePath + 'util/**/*.js'
+            jsBasePath + "*.js",
+            jsBasePath + "view/**/*.js",
+            jsBasePath + "util/**/*.js",
+            "Gruntfile.js"
         ];
 
     grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
+        pkg: grunt.file.readJSON("package.json"),
         jshint: {
             all: jsFiles,
             options: {
-                quotmark: "double",
-                curly: true,
-                browser: true,
-                laxbreak : true,
-                eqeqeq : true,
-                forin: true,
-                indent: 4,
-                newcap: true,
-                plusplus: true,
-                undef: true,
-                strict: true,
-                trailing: true,
-                devel: true,
-                globals: {
-                    require: false,
-                    define: false
+                jshintrc: ".jshintrc"
+            }
+        },
+        requirejs: {
+            compile: {
+                options: {
+                    baseUrl: "static-data/static/js",
+                    mainConfigFile: "static-data/static/js/main.js",
+                    dir: "static-data/build/js",
+                    name: "libs/require"
                 }
             }
         },
+        cssmin: {
+            minify: {
+                cwd: "static-data/static/css",
+                expand: true,
+                src: [
+                    "**/*.css"
+                ],
+                dest: "static-data/build/css/"
+            }
+        },
+        imagemin: {
+            minify: {
+                files: [{
+                    expand: true,
+                    cwd: "static-data/static/img",
+                    src: "{,*/}*.{png,jpg,jpeg}",
+                    dest: "static-data/build/img"
+                }]
+            }
+        },
+        watch: {
+            js: {
+                files: jsFiles,
+                tasks: [
+                    "jshint"
+                ]
+            }
+        }
     });
 
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-contrib-requirejs');
+    // Task that builds optimized versions of all static files and moves them
+    // into the static-data/build directory
+    grunt.registerTask("build", [
+        "requirejs",
+        "cssmin",
+        "imagemin"
+    ]);
 };
