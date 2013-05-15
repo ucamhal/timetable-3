9ded6a65f22565b94440ec6e72816d190b8df3c7
Hal Blackburn <hwtb2@caret.cam.ac.uk>
2013-05-15 11:58:55 +0100

    Update settings file to reflect preceding dir changes

--- app/django/timetables/settings/base_non_local.py
+++ app/django/timetables/settings/base_non_local.py
@@ -10,46 +10,38 @@ from logging.handlers import SysLogHandler
 from .config_loader_utility import load_external_config
 from .base import *
 
+DEBUG = False
+TEMPLATE_DEBUG = DEBUG
+
+ENABLE_RAVEN = True
+
+# Dump all SQL used in a request, if it exceeds thresholds or is requested.
+DUMP_FULL_SQL = False
+
+JSON_INDENT = 0
+
+MEDIA_ROOT = REPO_ROOT_DIR.child("data")
+STATIC_ROOT = REPO_ROOT_DIR.child("htdocs")
+
 # Sensitive settings which are loaded from a config file not in this repo
 ## FIXME use unipath
-EXTERNAL_CONFIG_FILE = ROOT_PATH + "/local/secret.conf"
+EXTERNAL_CONFIG_FILE = REPO_ROOT_DIR.child("local", "secret.conf")
 EXTERNAL_CONFIG = load_external_config(EXTERNAL_CONFIG_FILE)
 
-if 'test' in sys.argv or not PG_INSTALLED:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-            'NAME': "%s/app-data/data.db" % ROOT_PATH, # Or path to database file if using sqlite3.
-            'USER': '', # Not used with sqlite3.
-            'PASSWORD': '', # Not used with sqlite3.
-            'HOST': '', # Set to empty string for localhost. Not used with sqlite3.
-            'PORT': '', # Set to empty string for default. Not used with sqlite3.
-        }
-    }
 
-else:
-    
-    DATABASES = {
-        'default': {
-            "ENGINE": "django.db.backends.postgresql_psycopg2",
-            "NAME": EXTERNAL_CONFIG.get("database", "name"),
-            "USER": EXTERNAL_CONFIG.get("database", "user"),
-            "PASSWORD": EXTERNAL_CONFIG.get("database", "password"),
-            "HOST": EXTERNAL_CONFIG.get("database", "host"),
-            "PORT": EXTERNAL_CONFIG.get("database", "port"),
-            'OPTIONS': {
-                        'autocommit': True, # If you set this to False, a transaction will be created every time even if the app doesnt use it. Dont set it to False, transactions are managed differently.
-            }
-        },
-        'testing': {
-            'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-            'NAME': "%s/app-data/data.db" % ROOT_PATH, # Or path to database file if using sqlite3.
-            'USER': '', # Not used with sqlite3.
-            'PASSWORD': '', # Not used with sqlite3.
-            'HOST': '', # Set to empty string for localhost. Not used with sqlite3.
-            'PORT': '', # Set to empty string for default. Not used with sqlite3.
+DATABASES = {
+    'default': {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": EXTERNAL_CONFIG.get("database", "name"),
+        "USER": EXTERNAL_CONFIG.get("database", "user"),
+        "PASSWORD": EXTERNAL_CONFIG.get("database", "password"),
+        "HOST": EXTERNAL_CONFIG.get("database", "host"),
+        "PORT": EXTERNAL_CONFIG.get("database", "port"),
+        'OPTIONS': {
+                    'autocommit': True, # If you set this to False, a transaction will be created every time even if the app doesnt use it. Dont set it to False, transactions are managed differently.
         }
     }
+}
 
 SECRET_KEY = EXTERNAL_CONFIG.get("crypto", "secretkey")
 
@@ -58,4 +50,34 @@ AUTHENTICATION_BACKENDS = (
     'timetables.backend.TimetablesAuthorizationBackend',
 )
 
-
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "syslog": {
+            "format": "timetables: %(message)s",
+            "datefmt": "[%Y-%m-%d %H:%M:%S]"
+        }
+    },
+    "handlers": {
+        "syslog": {
+            "level": "DEBUG",
+            "class": "logging.handlers.SysLogHandler",
+            "facility": SysLogHandler.LOG_LOCAL6,
+            "address": "/dev/log",
+            "formatter": "syslog"
+        }
+    },
+    "loggers": {
+        "django.request": {
+            "handlers": ["syslog"],
+            "level": "ERROR",
+            "propagate": True,
+        },
+        "timetables": {
+            "handlers": ["syslog"],
+            "level": "DEBUG",
+            "propagate": True,
+        }
+    }
+}
