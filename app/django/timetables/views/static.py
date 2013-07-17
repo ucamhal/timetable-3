3db9abdb6340aaa35b6e6bafd695fd0cbe2c7ec2
hwtb2 <hwtb2@59899196-baf2-4bc4-a94d-4a08719e171f>
2013-07-17 10:53:05 +0000

    Merge branch feature/update-privacy-policy
    git-svn-id: https://source.caret.cam.ac.uk/svn/projects/timetables/trunk@17721 59899196-baf2-4bc4-a94d-4a08719e171f

--- app/django/timetables/views/static.py
+++ app/django/timetables/views/static.py
@@ -1,9 +1,15 @@
 from django import shortcuts
 from django.contrib.auth.decorators import login_required, permission_required
 
+from timetables.utils import site
+
 
 def cookie_policy(request):
-    return shortcuts.render(request, "static/cookie-policy.html")
+    return shortcuts.render(request, "static/cookie-policy.html", {
+        "site_url": site.get_site_url_from_request(request)
+    })
 
 def privacy_policy(request):
-    return shortcuts.render(request, "static/privacy-policy.html")
+    return shortcuts.render(request, "static/privacy-policy.html", {
+        "site_url": site.get_site_url_from_request(request)
+    })
