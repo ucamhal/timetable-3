8128169ca2f6b480d7a3968f4a2bb44599f8291f
hwtb2 <hwtb2@59899196-baf2-4bc4-a94d-4a08719e171f>
2013-08-30 16:40:57 +0000

    Update REMOTE_USER_HEADER setting for dev.
    git-svn-id: https://source.caret.cam.ac.uk/svn/projects/timetables/trunk@18633 59899196-baf2-4bc4-a94d-4a08719e171f

--- app/django/timetables/settings/dev.py
+++ app/django/timetables/settings/dev.py
@@ -13,3 +13,10 @@ WSGI_APPLICATION = "timetables.wsgi.dev.application"
 GOOGLE_ANALYTICS_ID = "UA-__addthis__"
 
 IS_PLAY_SITE = True
+
+# We're using gunicorn for dev now. Previously apache would set the
+# REMOTE_USER environ value directly, but now that apache sends that
+# along to gunicorn it's seen as a client supplied header, so prefixed
+# with HTTP_ in the wsgi environ, As a result we need to look for
+# HTTP_REMOTE_USER instead of REMOTE_USER.
+REMOTE_USER_HEADER = "HTTP_REMOTE_USER"
