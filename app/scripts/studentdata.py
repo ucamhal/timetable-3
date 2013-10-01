d27cb92cf7c1274519d54cc2893aafa40d935ee1
hwtb2 <hwtb2@59899196-baf2-4bc4-a94d-4a08719e171f>
2013-10-01 11:27:39 +0000

    Add academic plan alias to output to allow obfuscated analytics
    We'll send the alias to Google Analytics instead of the actual plan.
    
    git-svn-id: https://source.caret.cam.ac.uk/svn/projects/timetables/trunk@20427 59899196-baf2-4bc4-a94d-4a08719e171f

--- app/scripts/studentdata.py
+++ app/scripts/studentdata.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 """
 Utility to assist with joining CRSID data from education section with data from
-the camsis coding manual to create a mapping of CRSID -> Tripos (roughly). I say
-roughly because we map to "Academic Plans" which are not neccisarilly a 1:1 map
-to tripos...
+the camsis coding manual to create a mapping of CRSID -> Tripos (roughly). I
+say roughly because we map to "Academic Plans" which are not neccisarilly a 1:1
+map to tripos...
 
 To use this you'll need to run each of the 4 subcommands listed under usage.
 First to init a db with required tables, then 2 commands to load data, then the
@@ -14,7 +14,9 @@ Usage:
     studentdata.py init <db_path>
     studentdata.py load crsids <db_path> <crsids_csv_path>
     studentdata.py load camsis-codes <db_path> <camsis_codes_csv_path>
+    studentdata.py generate aliases <db_path> --seed=<seed> [--len=<n>]
     studentdata.py export json <db_path>
+    studentdata.py export csv <db_path>
 
 Arguments:
     db_path: Path to an sqlite 3 database to operate on.
@@ -23,26 +25,28 @@ Arguments:
 
                      The email addresses should be crsid@cam.ac.uk
     camsis_codes_csv_path: The CSV export from http://www.camsis.cam.ac.uk/cam-only/current_users/student-codes/
+    --len: The length (in characters) of the generated alias.
 """
 from __future__ import unicode_literals
 
-import sys
-import sqlite3
 import csv
-import re
+import hashlib
 import json
+import re
+import sqlite3
+import sys
 
+import base32_crockford
 import docopt
 
 
 class Command(object):
     create_table_sql = """
-    CREATE TABLE student (usn, name, academic_programme, academic_plan, admin_term, email, crsid);
-    CREATE UNIQUE INDEX student_crsid on student (crsid);
-    CREATE UNIQUE INDEX student_usn on student (usn);
+    CREATE TABLE student (usn UNIQUE, name, academic_programme, academic_plan, admin_term, email, crsid UNIQUE);
 
-    CREATE TABLE academic_plan (id, description, type, academic_programme, process_date);
-    CREATE UNIQUE INDEX academic_plan_id on academic_plan (id);
+    CREATE TABLE academic_plan (id UNIQUE, description, type, academic_programme, process_date);
+
+    CREATE TABLE plan_alias (academic_plan_id UNIQUE, alias UNIQUE, seed);
     """
 
     def __init__(self, args):
@@ -103,18 +107,80 @@ class Command(object):
                          academic_plans)
         conn.commit()
 
-    def export_json(self):
+    def get_export_cursor(self):
         conn = self.get_db_connection()
-        cursor = conn.execute("SELECT crsid, academic_plan.description, academic_plan.id "
-                              "FROM student JOIN academic_plan "
-                              "ON student.academic_plan = academic_plan.id "
+        return conn.execute("SELECT crsid, academic_plan.description, academic_plan.id, plan_alias.alias "
+                              "FROM student "
+                              "JOIN academic_plan ON student.academic_plan = academic_plan.id "
+                              "JOIN plan_alias ON student.academic_plan = plan_alias.academic_plan_id "
                               "WHERE student.crsid IS NOT NULL")
 
+    def export_json(self):
+        cursor = self.get_export_cursor()
+
         export_data = dict(
-            (crsid, {"academic_plan": description, "academic_plan_id": plan_id})
-            for (crsid, description, plan_id) in cursor
+            (crsid, {"academic_plan": description, "academic_plan_id": plan_id, "plan_alias": alias})
+            for (crsid, description, plan_id, alias) in cursor
         )
         json.dump(export_data, sys.stdout, indent=4)
+
+    def export_csv(self):
+        cursor = self.get_export_cursor()
+
+        rows = [["CRSID", "Academic Plan", "Academic Plan ID", "Plan Alias"]]
+        rows += map(list, cursor)
+
+        csv.writer(sys.stdout).writerows(rows)
+
+    def get_alias_seed(self):
+        seed = self.args["--seed"]
+        if not seed:
+            raise ValueError("A --seed value is required.")
+        return seed
+
+    def get_alias_len(self):
+        raw = self.args["--len"]
+        if raw is None:
+            return 6
+
+        alias_len = int(raw)
+        if alias_len < 1:
+            raise ValueError("--len must be > 0")
+        return alias_len
+
+    def generate_alias(self, plan):
+        md5 = hashlib.md5()
+        md5.update(self.get_alias_seed())
+        md5.update(plan[b"id"])
+        md5.update(plan[b"description"])
+        num = int(md5.hexdigest(), 16)
+        return base32_crockford.encode(num)[:self.get_alias_len()]
+
+    def detect_duplicates(self, aliases):
+        dupes = set()
+        count = 0
+        unique = set()
+        for _, alias, _ in aliases:
+            unique.add(alias)
+            if len(unique) == count:
+                dupes.add(alias)
+            count = len(unique)
+
+        if dupes:
+            raise ValueError("Duplicate aliases found", dupes)
+
+    def generate_aliases(self):
+        conn = self.get_db_connection()
+        plans = conn.execute("SELECT * FROM academic_plan")
+        aliases = [
+            (plan[b"id"], self.generate_alias(plan), self.get_alias_seed())
+            for plan in plans
+        ]
+
+        self.detect_duplicates(aliases)
+
+        conn.executemany("INSERT INTO plan_alias VALUES (?,?,?)", aliases)
+        conn.commit()
 
 
 def filter_camsis_csv_to_section(section, data):
@@ -149,8 +215,16 @@ def load_camsis_codes(args):
     Command(args).load_camsis_codes()
 
 
+def generate_aliases(args):
+    Command(args).generate_aliases()
+
+
 def export_json(args):
     Command(args).export_json()
+
+
+def export_csv(args):
+    Command(args).export_csv()
 
 
 if __name__ == "__main__":
@@ -162,7 +236,11 @@ if __name__ == "__main__":
         load_crsids(args)
     elif args["load"] and args["camsis-codes"]:
         load_camsis_codes(args)
-    elif args["export"]:
+    elif args["generate"] and args ["aliases"]:
+        generate_aliases(args)
+    elif args["export"] and args["json"]:
         export_json(args)
+    elif args["export"] and args["csv"]:
+        export_csv(args)
     else:
         assert False, ("Not all argument cases handled.", args)
