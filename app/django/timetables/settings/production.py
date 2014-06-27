eb88f752e2c12bb00b7e2364871a1c768df7f5e1
hwtb2 <hwtb2@59899196-baf2-4bc4-a94d-4a08719e171f>
2014-06-27 09:29:38 +0000

    Mark the 2013-2014 production & play as being archived
    git-svn-id: https://source.caret.cam.ac.uk/svn/projects/timetables/trunk@24132 59899196-baf2-4bc4-a94d-4a08719e171f

--- app/django/timetables/settings/production.py
+++ app/django/timetables/settings/production.py
@@ -1,6 +1,8 @@
 """
 This is the settings file used for the 'production' class server in the
 dev, qa, staging, production stack of servers.
+
+This is the 2013-2014 production server
 """
 # Extend default settings from base_non_local.py
 from .base_non_local import *
@@ -14,3 +16,6 @@ ALLOWED_HOSTS = [
 WSGI_APPLICATION = "timetables.wsgi.production.application"
 
 GOOGLE_ANALYTICS_ID = "UA-43714583-1"
+
+# This site is now archived.
+NEXT_YEAR_SITE_URL = "https://2014.timetable.cam.ac.uk/administration"
--- app/django/timetables/settings/production_play.py
+++ app/django/timetables/settings/production_play.py
@@ -1,6 +1,8 @@
 """
 This is the settings file used for the "play" (sandbox) 'production' class
 server in the dev, qa, staging, production stack of servers.
+
+This is the 2013-2014 play server.
 """
 # Extend default settings from base_non_local.py
 from .base_non_local import *
@@ -17,3 +19,6 @@ GOOGLE_ANALYTICS_ID = "UA-43714583-2"
 
 # Use the beta banner
 IS_PLAY_SITE = True
+
+# This site is now archived.
+NEXT_YEAR_SITE_URL = "https://play-2014.timetable.cam.ac.uk/administration"
